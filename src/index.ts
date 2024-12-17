dea387ca9f46837d8311e582b38127a97121f0dd
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-12-17 19:04:56 -0300

    adiciona atualizacao

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
-import express from 'express'
-
+import express from 'express';
+import { AppDataSource } from './database/data-source';
 
 /*
-
-get -> buscar informação
-post -> enviar informação e criar uma informacao
-delete -> deletar informação
-put -> atualizar informação
+    get -> buscar informação
+    post -> enviar informação e criar uma informacao
+    delete -> deletar informação
+    put -> atualizar informação
 */
 
-const app = express()
+const app = express();
 
-app.get("/bem-vindo", () => {
-    console.log("Seja bem-vindo")
-})
+AppDataSource.initialize()
+    .then(() => {
+        console.log('Conectado ao banco de dados');
+    })
+    .catch(error => {
+        console.error('Erro ao conectar ao banco de dados', error);
+    });
 
+app.get('/bem-vindo', (request, response) => {
+    //response.send("<strong>Olá, mundo!</strong>")
+    response.json({ message: 'Ola, mundo' });
+});
 
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000')
-} )
-
+    console.log('Servidor rodando na porta 3000');
+});
